6456159170689fce4616fba8883ad688d6e7c967
Juhyeong Park <neverdie0304@naver.com>
2024-08-22 16:37:56 +0100

    Created Complete and Exit button on GUI

--- toDoGUI.py
+++ toDoGUI.py
@@ -7,10 +7,14 @@ add_button = gui.Button("Add")
 list_box = gui.Listbox(values=functions.get_todos(), key='todos',
                        enable_events=True, size=[45,10])
 edit_button = gui.Button("Edit")
-
+complete_button = gui.Button("Complete")
+exit_button = gui.Button("Exit")
 
 window = gui.Window("My To-Do App", # This will be the title of the App.
-                    layout=[[label], [input_box, add_button], [list_box, edit_button]], # list inside list means a row. layout=[[row1], [row2]]
+                    layout=[[label], # row 1 on the window
+                            [input_box, add_button], # row 2 on the window ...
+                            [list_box, edit_button, complete_button],
+                            [exit_button]], 
                     font=('Helvetica', 20)) 
 
 # window.read()  # user action required to execute below this line.
@@ -27,6 +31,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value="")
 
         case "Edit":
             todo_to_edit = values['todos'][0]
@@ -37,6 +42,17 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+
+        case "Exit":
+            break
 
         case "todos":
             window['todo'].update(value=values['todos'][0])
