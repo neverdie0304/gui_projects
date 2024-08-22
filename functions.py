953521ce716817bae26517b8255635f423150eac
Juhyeong Park <neverdie0304@naver.com>
2024-08-22 15:41:26 +0100

    Modification made in to-do project

---
+++ functions.py
@@ -0,0 +1,11 @@
+FILEPATH = "./todo.txt"
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of to-do items."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
--- toDoGUI.py
+++ toDoGUI.py
@@ -1,10 +1,29 @@
 import FreeSimpleGUI as gui
-
+import functions
 
 label = gui.Text("Type in a to-do")
-input_box = gui.InputText(tooltip="Enter todo") # tooltip is a message shown on hover
+input_box = gui.InputText(tooltip="Enter todo", key="todo") # tooltip is a message shown on hover
 add_button = gui.Button("Add")
 
-window = gui.Window("My To-Do App", layout=[[label, input_box], [add_button]]) # This will be the title of the App. list inside list means a one row. layout=[[row1], [row2]]
-window.read()  # user action required to execute below this line.
+window = gui.Window("My To-Do App", # This will be the title of the App.
+                    layout=[[label], [input_box, add_button]], # list inside list means a row. layout=[[row1], [row2]]
+                    font=('Helvetica', 20)) 
+
+# window.read()  # user action required to execute below this line.
+# Alternatively
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        # In case it's an add event
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        
+        case gui.WIN_CLOSED:
+            break
+
 window.close()
